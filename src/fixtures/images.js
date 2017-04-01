1059e3db870737d84863476621ec7b3723a1a196
walker flynn <walker.d.flynn@gmail.com>
2017-04-01 19:01:07 -0400

    Update images.js

--- src/fixtures/images.js
+++ src/fixtures/images.js
@@ -1,5 +1,72 @@
 window.manifestArt = [
   {
+    image: "https://s3.amazonaws.com/lydia-enriquez-art/art/lydia11+copy.jpg",
+    title: "Double Vein",
+    dimensions: {
+      width: 36,
+      height: 36,
+      units: "inches"
+    }
+    materials: "plaster, paint, mica powder, and wax on panel with carving",
+    year: 2017
+  },
+    {
+    image: "https://s3.amazonaws.com/lydia-enriquez-art/art/marble.jpg",
+    title: "untitled tomne",
+    dimensions: {
+      width: 25,
+      height: 25,
+      depth: 5,
+      units: "inches"
+    }
+    materials: "plaster, tint and wax on shaped panel",
+    year: 2017
+  },
+   {
+    image: "https://s3.amazonaws.com/lydia-enriquez-art/art/lydia1.jpg",
+    title: "Solidifying the Ghost",
+    dimensions: {
+      width: 48,
+      height: 54,
+      units: "inches"
+    }
+    materials: "plaster, paint, and mica powder on panel",
+    year: 2017
+  },
+  {
+    image: "https://s3.amazonaws.com/lydia-enriquez-art/art/lydia4+copy.jpg",
+    title: "Untitled",
+    dimensions: {
+      width: 16,
+      height: 20,
+      units: "inches"
+    }
+    materials: "plaster, resin and moon gold on panel",
+    year: 2017
+  },
+  {
+    image: "https://s3.amazonaws.com/lydia-enriquez-art/art/lydia6+copy.jpg",
+    title: "Holy but not Reverent",
+    dimensions: {
+      width: 36,
+      height: 48,
+      units: "inches"
+    }
+    materials: "dirt, resin and moon gold on panel",
+    year: 2017
+  },
+   {
+    image: "ttps://s3.amazonaws.com/lydia-enriquez-art/art/lydia7+copy.jpg",
+    title: "untitled",
+    dimensions: {
+      width: 24,
+      height: 18,
+      units: "inches"
+    }
+    materials: "paint, mica powder and wax on panel",
+    year: 2017
+  },
+   {
     image: "https://s3.amazonaws.com/lydia-enriquez-art/art/Memenomori.jpg",
     title: "Memento Mori",
     dimensions: {
@@ -20,22 +87,47 @@ window.manifestArt = [
     }
     materials: "plaster, paint, and mica powder on panel",
     year: 2015
-  }
+  },
+   {
+    image: "https://s3.amazonaws.com/lydia-enriquez-art/art/lydia5.jpg",
+    title: "untitled nocturne",
+    dimensions: {
+      width: 24,
+      height: 24,
+      units: "inches"
+    }
+    materials: "paint, mica powder and wax on panel",
+    year: 2017
+  },
 ];
 
-window.manifestInstallations = [
-   {
-    image: "https://s3.amazonaws.com/lydia-enriquez-art/installations/16livingroom.jpg",
-    title: "commissoned plaster for tribeca loft",
-    dimensions: {},
-    materials: "plaster and paint on wall",
-    year: 2015
-  }, 
+window.manifestInstallations = [ 
    {
     image: "https://s3.amazonaws.com/lydia-enriquez-art/installations/install.jpg",
     title: "Fervent Green",
     dimensions: {},
     materials: "an interior installation with wall, floor and hung components",
     year: 2016
+  }, 
+   {
+    image: "https://s3.amazonaws.com/lydia-enriquez-art/installations/FinalMerit8+copy.jpg",
+    title: "Fervent Green, Detail",
+    dimensions: {},
+    materials: "an interior installation with wall, floor and hung components",
+    year: 2016
+  },
+    {
+    image: "https://s3.amazonaws.com/lydia-enriquez-art/installations/16livingroom.jpg",
+    title: "commissoned plaster for tribeca loft",
+    dimensions: {},
+    materials: "plaster and paint on wall",
+    year: 2015
+  },
+   {
+    image: "https://s3.amazonaws.com/lydia-enriquez-art/installations/14realfauxbois.jpg",
+    title: "Real Faux Bois",
+    dimensions: {},
+    materials: "objects and background, plaster, paint, mica powder and wax",
+    year: 2015
   }
 ]
